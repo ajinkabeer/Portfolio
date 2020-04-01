f5aa9cc2e35657e0af2d86bfddb486fcc17363b9
ajinkabeer <ajiinkabeer@gmail.com>
2020-04-02 00:52:55 +0530

    Add localstorage to save mode pref

--- js/index.js
+++ js/index.js
@@ -1,49 +1,54 @@
 const button = document.getElementById("dark-mode");
-const anchor = document.querySelectorAll('.nav-link-anchor')
-const body = document.getElementsByTagName("body")[0]
-const h1 = document.getElementById('heading-name')
-const h3 = document.getElementById('heading-tag')
+const anchor = document.querySelectorAll(".nav-link-anchor");
+const body = document.getElementsByTagName("body")[0];
+const h1 = document.getElementById("heading-name");
+const h3 = document.getElementById("heading-tag");
+const lastMode = localStorage.getItem("mode");
+const white = "#ffffff";
+const black = "#000000";
+const dark = "#272727";
+const primary = "#ef4836";
 
-const white = '#ffffff'
-const black = '#000000'
-const dark = '#272727'
-const primary = "#ef4836"
-
-button.addEventListener('click', () => {
- if(button.innerText === 'Dark Mode'){
-    darkMode()
- }
- else {
-    lightMode()
- }
-})
-
-function darkMode(){
-    button.innerText = 'Light Mode';
-    body.style.backgroundColor = dark;
-    h1.style.color = white;
-    h3.style.color = white;
-    anchor.forEach(item=>{
-        if(item.id.includes('active')){
-            item.style.color=primary;
-        }
-        else {
-            item.style.color = white;
-        }
-    })
+if (lastMode === "dark") {
+  darkMode();
+} else {
+  lightMode();
 }
 
-function lightMode(){
-    button.innerText = 'Dark Mode';
-    body.style.backgroundColor = white;
-    h1.style.color = black
-    h3.style.color = black
-    anchor.forEach(item=>{
-        if(item.id.includes('active')){
-            item.style.color=primary
-        }
-        else {
-            item.style.color = black;
-        }
-    })
+button.addEventListener("click", () => {
+  if (button.innerText === "Dark") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  localStorage.setItem("mode", "dark");
+  button.innerText = "Light";
+  body.style.backgroundColor = dark;
+  h1.style.color = white;
+  h3.style.color = white;
+  anchor.forEach(item => {
+    if (item.id.includes("active")) {
+      item.style.color = primary;
+    } else {
+      item.style.color = white;
+    }
+  });
+}
+
+function lightMode() {
+  localStorage.setItem("mode", "light");
+  button.innerText = "Dark";
+  body.style.backgroundColor = white;
+  h1.style.color = black;
+  h3.style.color = black;
+  anchor.forEach(item => {
+    if (item.id.includes("active")) {
+      item.style.color = primary;
+    } else {
+      item.style.color = black;
+    }
+  });
 }
